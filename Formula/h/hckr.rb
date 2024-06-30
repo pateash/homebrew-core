52048c65016773e4743453209a4da49e853d89fe
Ashish Patel <ashishpatel0720@gmail.com>
2024-06-30 22:12:28 +0530

    [brew]: tests added

--- Formula/h/hckr.rb
+++ Formula/h/hckr.rb
@@ -1,7 +1,7 @@
 class Hckr < Formula
   include Language::Python::Virtualenv
 
-  desc "hckr - Awesome CLI for developers"
+  desc "Hckr - Awesome CLI for developers"
   homepage "https://pypi.org/project/hckr/"
   url "https://files.pythonhosted.org/packages/source/h/hckr/hckr-0.2.5.tar.gz"
   sha256 "ec4fd367818f26a459bd98db04b49f985800a45716a1f1a5678a202e2aadcc80"
