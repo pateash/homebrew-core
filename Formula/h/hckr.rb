3371590e80ac21d919e2a6a7782f057cc1b4588a
Ashish Patel <ashishpatel0720@gmail.com>
2024-07-01 20:02:12 +0530

    [brew]: tests added

--- Formula/h/hckr.rb
+++ Formula/h/hckr.rb
@@ -1,12 +1,11 @@
 class Hckr < Formula
   include Language::Python::Virtualenv
 
-  desc "Hckr - Awesome CLI for developers"
+  desc "Awesome CLI hack for developers"
   homepage "https://pypi.org/project/hckr/"
-  url "https://files.pythonhosted.org/packages/source/h/hckr/hckr-0.2.5.tar.gz"
+  url "https://files.pythonhosted.org/packages/01/3f/525f57e9e3cdcc91383e0f3106b3003a8b7bc5ab1aa3b6c90ffbdf526647/hckr-0.2.5.tar.gz"
   sha256 "ec4fd367818f26a459bd98db04b49f985800a45716a1f1a5678a202e2aadcc80"
   license "MIT"
-
   depends_on "python@3.12"
 
   def install
@@ -18,6 +17,7 @@ class Hckr < Formula
 
   test do
     (testpath/"foo.txt").write("Nobody inspects the spammish repetition\n")
-    assert_match "SHA1: 55cd84c899a3d9f41d5a4d1be4ac57b37551db00", shell_output("#{bin}/hckr hash sha1 -f #{testpath}/foo.txt")
+    assert_match "SHA1: 55cd84c899a3d9f41d5a4d1be4ac57b37551db00",
+                 shell_output("#{bin}/hckr hash sha1 -f #{testpath}/foo.txt")
   end
 end
